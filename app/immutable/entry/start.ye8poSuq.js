b2237258c3b90dad5d565339d7c5b02c70ee188d
joshtune <joshtune@users.noreply.github.com>
2024-10-25 22:29:48 +0000

    deploy: 2eb95cc6a96776c8232c110b13ad3c8e5537be94

---
+++ app/immutable/entry/start.ye8poSuq.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Bpq6o9ef.js";export{t as start};
---
+++ app/immutable/nodes/4.AdIfqwD-.js
@@ -0,0 +1,1 @@
+import{s as t}from"../chunks/scheduler.CPRCFwWh.js";import{S as o,i as r}from"../chunks/index.BR-d8M_U.js";const n=!0,p=Object.freeze(Object.defineProperty({__proto__:null,prerender:n},Symbol.toStringTag,{value:"Module"}));class u extends o{constructor(e){super(),r(this,e,null,null,t,{})}}export{u as component,p as universal};
